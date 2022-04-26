04504bbbd0eb8d5a45581bb6c62daab21c765ba8
Jordi <j.z.cuffi@gmail.com>
2022-04-26 15:31:16 +0200

    training

--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -20,12 +20,9 @@ from pages.views import home_view, contact_view, about_view
 
 
 urlpatterns = [
-    path('blog/', include('blog.urls')),
-    path('courses/', include('courses.urls')),
-    path('products/', include('products.urls')),
     path('', home_view, name='home'),
-    path('about/<int:id>/', about_view, name='product-detail'),
-    path('contact/', contact_view),
+    path('contact/', contact_view, name='contact'),
+    path('about/', about_view, name='about'),
     path('admin/', admin.site.urls),
 ]
 
